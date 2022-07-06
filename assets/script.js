96eb8d25c2ef4de3bfdb5c306fd5c36927938395
Jun Hwang <jhwang2525@gmail.com>
2022-07-05 22:43:24 -0700

    tasks function

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+var formEl = document.querySelector("#task-form");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+var taskIdCounter = 0;
+var pageContentEl = document.querySelector("#page-content");
+var tasksInProgressEl = document.querySelector("#tasks-in-progress");
+var tasksCompletedEl = document.querySelector("#tasks-completed");
+
+var tasks = [];
